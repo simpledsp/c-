607974e191682a79091c976512c43868f0827000
simpledsp <52387135+simpledsp@users.noreply.github.com>
2019-07-20 10:40:33 -0400

    Create intVecToTree.cpp

---
+++ fun/intVecToTree.cpp
@@ -0,0 +1,18 @@
+#define SIMPLEDSP
+#include "cheader.h"
+//do not use define or include any header file
+
+//add your functions here
+
+//main function, do not change function name
+int main(int argc, char* argv[])
+{
+    //call your function here
+    TreeNode* head=intVecToTree({1,2, 3,-1,-1, 5});
+    //ahd(head)
+    TreeNode* p=head->left;
+    //a(p)
+    //lk("head", p)
+    //dsp
+    return 0;
+}            
