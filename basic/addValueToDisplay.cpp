00447282e5c35481d25061bd28f105dd939b902e
simpledsp <52387135+simpledsp@users.noreply.github.com>
2019-07-20 10:45:32 -0400

    Create addValueToDisplay.cpp

---
+++ basic/addValueToDisplay.cpp
@@ -0,0 +1,17 @@
+#define SIMPLEDSP
+#include "cheader.h"
+//do not use define or include any header file
+
+//add your functions here
+
+//main function, do not change function name
+int main(int argc, char* argv[])
+{
+    int a=10;
+    //a(a)
+    for(int i=0; i<10; ++i){
+        ++a;
+        //dsp
+    }
+    return 0;
+}            
