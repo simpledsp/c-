ce4e88541c7a81e0bb310ed4ce18eec97f3beddd
simpledsp <52387135+simpledsp@users.noreply.github.com>
2019-07-20 11:09:12 -0400

    Create addMaptoDisplay.cpp

---
+++ basic/addMaptoDisplay.cpp
@@ -0,0 +1,15 @@
+#define SIMPLEDSP
+#include "cheader.h"
+//do not use define or include any header file
+
+//add your functions here
+
+//main function, do not change function name
+int main(int argc, char* argv[])
+{
+    //call your function here
+    map<int, pair<int,string>> mp = {{1,{1,""}}, {2,{10,"abc"}}};
+    //amap(mp, int, pair<int,string>)
+    //dsp
+    return 0;
+}            
