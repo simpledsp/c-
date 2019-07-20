f61925ed4d3170cfab2f003d5152bcba240d39f8
simpledsp <52387135+simpledsp@users.noreply.github.com>
2019-07-20 10:36:00 -0400

    Create intVecToList.cpp

---
+++ fun/intVecToList.cpp
@@ -0,0 +1,18 @@
+#define SIMPLEDSP
+#include "cheader.h"
+//do not use define or include any header file
+
+//add your functions here
+
+//main function, do not change function name
+int main(int argc, char* argv[])
+{
+    //call your function here
+    ListNode* head=intVecToList({1,2,3,4,5});
+    //ahd(head)
+    ListNode* p=head->next;
+    //a(p)
+    //lk(head, p)
+    //dsp
+    return 0;
+}            
